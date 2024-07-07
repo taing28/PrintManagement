d99fb037b511d72bcfe08ab863e3c6dd198ae8fa
taing28 <taing280703@gmail.com>
2024-07-07 23:56:57 +0700

    add entity

---
+++ PrintManagement/src/main/java/org/example/printmanagement/model/entities/User.java
@@ -0,0 +1,82 @@
+package org.example.printmanagement.model.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String userName;
+    private String password;
+    private String fullName;
+    private LocalDate dateOfBirth;
+    private String avatar;
+    private String email;
+    private LocalDateTime createTime = LocalDateTime.now();
+    private LocalDateTime updateTime = LocalDateTime.now();
+    private String phoneNumber;
+    @Column(insertable = false, updatable = false)
+    private int teamId;
+    private boolean isActive = true;
+
+    public User(int id) {
+        this.id = id;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "teamId", foreignKey = @ForeignKey(name = "fk_user_team"))
+    @JsonIgnore
+    private Team team;
+
+    @OneToMany(mappedBy = "userNotify", fetch = FetchType.EAGER)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private List<Notification> notificationList;
+
+    @OneToMany(mappedBy = "employeeBill", fetch = FetchType.EAGER)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private List<Bill> billList;
+
+    @OneToMany(mappedBy = "", fetch = FetchType.EAGER)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private List<> ;
+
+    @OneToMany(mappedBy = "", fetch = FetchType.EAGER)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private List<> ;
+
+    @OneToMany(mappedBy = "", fetch = FetchType.EAGER)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private List<> ;
+
+    @OneToMany(mappedBy = "", fetch = FetchType.EAGER)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private List<> ;
+
+    @OneToMany(mappedBy = "", fetch = FetchType.EAGER)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private List<> ;
+
+    @OneToMany(mappedBy = "", fetch = FetchType.EAGER)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private List<> ;
+}
