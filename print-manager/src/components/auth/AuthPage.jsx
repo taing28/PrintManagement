2319ccbf982bc0cce894c6fe47ac97217ffd7f04
taing28 <taing280703@gmail.com>
2024-07-12 08:00:36 +0700

    Responsive form in auth page

--- print-manager/src/components/auth/AuthPage.jsx
+++ print-manager/src/components/auth/AuthPage.jsx
@@ -3,8 +3,10 @@ import { Outlet } from "react-router-dom";
 
 export const AuthPage = memo(() => {
     return (
-        <div className="wrapper">
-        <Outlet/>
+        <div className="wrapper-container">
+            <div className="wrapper">
+                <Outlet />
+            </div>
         </div>
     )
 })
