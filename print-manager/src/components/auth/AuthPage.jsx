3848b1276971b5041f0b39e78048fca2e87fff3b
taing28 <taing280703@gmail.com>
2024-07-12 11:52:17 +0700

    responsive for form

--- print-manager/src/components/auth/AuthPage.jsx
+++ print-manager/src/components/auth/AuthPage.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 export const AuthPage = memo(() => {
     return (
         <div className="wrapper-container">
-            <div className="wrapper">
+            <div className="wrapper mt-4 mb-4">
                 <Outlet />
             </div>
         </div>
