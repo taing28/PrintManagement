d1975c0f76d887866121441613b80bc89b4a31c6
taing28 <taing280703@gmail.com>
2024-07-21 14:39:31 +0700

    change Team method

--- print-manager/src/components/layouts/User.jsx
+++ print-manager/src/components/layouts/User.jsx
@@ -1,12 +1,31 @@
-import { message, Popconfirm, Row } from "antd";
+import { Form, Input, message, Modal, Popconfirm, Row, Select } from "antd";
 import { Table } from "react-bootstrap"
 import Search from "antd/es/transfer/search";
-import { memo, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import axiosInstance from "../config/AxiosConfig";
 
 export const User = memo(() => {
     const [userList, setUserList] = useState([]);
+    const [teamList, setTeamList] = useState([]);
+    const [submitStatus, setSubmitStatus] = useState(false);
+    const [formTeam] = Form.useForm();
 
+    // Fetch teams
+    useEffect(() => {
+        const fetchTeams = async () => {
+            try {
+                const response = await axiosInstance.get('/teams');
+                console.log('Teams', response.data);
+                setTeamList(response.data)
+            } catch (err) {
+                message.error(err.response?.data || 'Error fetching teams');
+            }
+        }
+
+        fetchTeams();
+    }, [])
+
+    //Fetch users
     const fetchUsers = async () => {
         try {
             const response = await axiosInstance.get('/users');
@@ -37,6 +56,47 @@ export const User = memo(() => {
     const cancel = (e) => {
         console.log(e);
         message.error('Click on No');
+    };
+
+    //Handle Change Team
+    const [isModalTeamOpen, setIsModalTeamOpen] = useState(false);
+    const showModalTeam = (user) => {
+        formTeam.setFieldsValue({
+            userId: user.id,
+            teamId: user.teamId,
+        });
+        setIsModalTeamOpen(true);
+    };
+
+    const submitTeam = async (values) => {
+        try {
+            setSubmitStatus(true);
+
+            const response = await axiosInstance.put('/users/change-team', values, {
+                params: {
+                    userId: values.userId,
+                    teamId: values.teamId,
+                },
+            });
+
+            // Kiểm tra trạng thái thành công từ server
+            if (response.status === 200) {
+                fetchUsers();
+                setIsModalTeamOpen(false);
+            } else {
+                // Xử lý khi có lỗi từ server, ví dụ hiển thị thông báo lỗi
+                message.error('Change failed. Please try again.');
+            }
+        } catch (error) {
+            console.error('Error:', error); // Handle error
+            message.error(error.response.data);
+        } finally {
+            setSubmitStatus(false); // Kết thúc trạng thái loading sau khi hoàn thành yêu cầu
+        }
+    }
+
+    const handleTeamCancel = () => {
+        setIsModalTeamOpen(false);
     };
 
     return (
@@ -79,7 +139,7 @@ export const User = memo(() => {
                                 <td>
                                     <div className="d-flex justify-content-evenly">
                                         <button className="btn btn-warning ">Role</button>
-                                        <button className="btn btn-success">Team</button>
+                                        <button className="btn btn-success" onClick={() => showModalTeam(user)}>Team</button>
                                         <Popconfirm
                                             title="Disable the user"
                                             description="Are you sure to ban this user?"
@@ -97,7 +157,7 @@ export const User = memo(() => {
                     })}
                 </tbody>
             </Table>
-            <h3 style={{color:'#fff'}}>Disabled users:</h3>
+            <h3 style={{ color: '#fff' }}>Disabled users:</h3>
             <Table className="mt-3" striped bordered hover variant="dark">
                 <thead>
                     <tr>
@@ -139,6 +199,58 @@ export const User = memo(() => {
                     })}
                 </tbody>
             </Table>
+
+            <Modal title="Change Team" open={isModalTeamOpen} onCancel={handleTeamCancel} footer={null}>
+                <Form name="team-form"
+                    labelCol={{
+                        span: 6,
+                    }}
+                    wrapperCol={{
+                        span: 14,
+                    }}
+                    layout="horizontal"
+
+                    form={formTeam}
+                    onFinish={submitTeam}
+                >
+                    <Form.Item
+                        label="Id"
+                        name="userId"
+                        hidden
+                    >
+                        <Input disabled />
+                    </Form.Item>
+
+                    <Form.Item label="Team"
+                        name="teamId"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please choose your team!',
+                            },
+                        ]}
+                    >
+                        <Select>
+                            {
+                                teamList.map((team, indexTeam) => {
+                                    return (
+                                        <Select.Option key={indexTeam} value={team.id}>{team.name}</Select.Option>
+                                    )
+                                })
+                            }
+                        </Select>
+                    </Form.Item>
+
+                    <Row className="justify-content-center">
+                        <button type="submit" className="btn btn-primary card-button" disabled={submitStatus}>
+                            Submit
+                        </button>
+                        <button type="button" className="btn btn-secondary card-button" onClick={handleTeamCancel} disabled={submitStatus}>
+                            Cancel
+                        </button>
+                    </Row>
+                </Form>
+            </Modal>
         </div>
     )
 })
