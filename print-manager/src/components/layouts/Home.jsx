85ec9a74be296047a92d7a65fd551914adecba6a
taing28 <taing280703@gmail.com>
2024-08-02 03:52:58 +0700

    Homepage fill with designs

--- print-manager/src/components/layouts/Home.jsx
+++ print-manager/src/components/layouts/Home.jsx
@@ -1,22 +1,42 @@
-import { memo } from "react";
-import { useUser } from "../config/UserContext";
+import { memo, useCallback, useEffect, useState } from "react";
+import { Card, Col, Row } from "react-bootstrap";
+import { message } from "antd";
+import axiosInstance from "../config/AxiosConfig";
 
 export const Home = memo(() => {
-  const { user } = useUser();
-  
+  const [designs, setDesigns] = useState([]);
+
+  //Take designs
+  const fetchDesign = useCallback(async () => {
+    try {
+      const response = await axiosInstance.get('/designs');
+      console.log('Designs: ', response.data);
+      setDesigns(response.data)
+
+    } catch (err) {
+      message.error(err.response?.data || 'Error fetching designs');
+    }
+  }, [])
+
+  useEffect(() => {
+    fetchDesign();
+  }, [fetchDesign]);
+
   return (
     <div>
-      {user && user.authorities ? (
-        <div>
-          <p>
-            Is Admin: {user.authorities.some((value) => {
-              return value === 'ROLE_ADMIN';
-            }) ? 'YES' : 'No'}
-          </p>
-        </div>
-      ) : (
-        <p>Đang tải dữ liệu user...</p>
-      )}
+        <Row>
+          {
+            designs.map((design, index) => {
+              return (
+                <Col key={index} lg={3} className="pt-3">
+                  <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={design.filePath} />
+                  </Card>
+                </Col>
+              )
+            })
+          }
+        </Row>
     </div>
-  );
+  )
 });
